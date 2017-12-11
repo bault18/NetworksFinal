efc14b812be36171890107183513f871cdb1e92d
bault18 <bault18@LAB-SCI-214-13>
2017-12-11 15:06:03 -0800

    Add project files.

---
+++ NetworksFinal/Classes.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+
+namespace NetworksFinal
+{
+    //All credit to Forgen on stack overflow for the TraceRoute class
+    //https://stackoverflow.com/questions/142614/traceroute-and-ping-in-c-sharp
+    public class TraceRoute
+    {
+        private const string Data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+
+        public static IEnumerable<IPAddress> GetTraceRoute(string hostNameOrAddress)
+        {
+            return GetTraceRoute(hostNameOrAddress, 1);
+        }
+        private static IEnumerable<IPAddress> GetTraceRoute(string hostNameOrAddress, int ttl)
+        {
+            Ping pinger = new Ping();
+            PingOptions pingerOptions = new PingOptions(ttl, true);
+            
+            int timeout = 1000;
+            byte[] buffer = Encoding.ASCII.GetBytes(Data);
+            PingReply reply = default(PingReply);
+
+            List<IPAddress> result = new List<IPAddress>();
+            try {
+                reply = pinger.Send(hostNameOrAddress, timeout, buffer, pingerOptions);
+            } catch (PingException exp) {
+                return result;
+            }
+
+
+            if (reply.Status == IPStatus.Success) {
+                result.Add(reply.Address);
+            }
+            else if (reply.Status == IPStatus.TtlExpired || reply.Status == IPStatus.TimedOut) {
+                //add the currently returned address if an address was found with this TTL
+                if (reply.Status == IPStatus.TtlExpired)
+                    result.Add(reply.Address);
+                //recurse to get the next address...
+                IEnumerable<IPAddress> tempResult = default(IEnumerable<IPAddress>);
+                tempResult = GetTraceRoute(hostNameOrAddress, ttl + 1);
+                result.AddRange(tempResult);
+            }
+            else {
+                //failure 
+            }
+
+
+            return result;
+        }
+    }
+
+    class Path
+    {
+        #region member variables
+        //stores list of coordinate locations
+        private List<Tuple<double, double>> CoordPath;
+        #endregion
+
+        //constructor
+        public Path() { CoordPath = new List<Tuple<double, double>>(); }
+
+        #region Get/Set Functions
+
+        //OUT: # of coordinates saved in the object
+        /// <summary>
+        /// Returns the number off lat,lng coords currently saved
+        /// </summary>
+        /// <returns> # of lat,lng coords in CoordPath</returns>
+        public int getNumCoords()
+        { return CoordPath.Count; }
+
+        /// <summary>
+        /// Adds a set of coordinates to the object's path
+        /// </summary>
+        /// <param name="lat">latitude to add to path</param>
+        /// <param name="lon">longitude to add to path</param>
+        public void AddLoc(double lat, double lon)
+        { CoordPath.Add(Tuple.Create(lat, lon)); }
+
+        /// <summary>
+        /// Grabs the first coordinate in the list and outputs the lat/lng in an str formatted for a google map pin
+        /// </summary>
+        /// <returns>str "lat: ____, lng: ____"</returns>
+        public string getStartLocStr()
+        { return "lat: " + Convert.ToString(CoordPath[0].Item1) + ", lng: " + Convert.ToString(CoordPath[0].Item2); }
+
+        /// <summary>
+        /// Grabs the last coordinate in the list and outputs the lat/lng in an str formatted for a google map pin
+        /// </summary>
+        /// <returns>str "lat: ____, lng: ____"</returns>
+        public string getEndLocStr()
+        { return "lat: " + Convert.ToString(CoordPath[CoordPath.Count - 1].Item1) + ", lng: " + Convert.ToString(CoordPath[CoordPath.Count - 1].Item2); }
+
+        /// <summary>
+        /// Converts all coordinate points in CoordPath into google polyline format
+        /// Lines will be created between each adjacent item in CoordPath
+        /// </summary>
+        /// <returns>Google API format for polyline path</returns>
+        public string getPolylineData()
+        {
+            string PathStr = String.Empty;
+            foreach (Tuple<double, double> CurrLoc in CoordPath)
+                PathStr += "{lat: " + Convert.ToString(CurrLoc.Item1) + ", lng: " + Convert.ToString(CurrLoc.Item2) + "},\n";
+
+            //remove comma/endline from final item in str
+            PathStr = PathStr.Remove(PathStr.Length - 2, 2);
+            return PathStr;
+        }
+
+        /// <summary>
+        /// Converts all coordinate points in CoordPath into google heat-map format
+        /// </summary>
+        /// <returns>Google API format for heat-map points</returns>
+        public string getHeatmapData()
+        {
+            string PathStr = "";
+            foreach (Tuple<double, double> CurrLoc in CoordPath)
+                PathStr += "new google.maps.LatLng(" + Convert.ToString(CurrLoc.Item1) + ", " + Convert.ToString(CurrLoc.Item2) + "),\n";
+
+            PathStr = PathStr.Remove(PathStr.Length - 2, 2);
+            return PathStr;
+        }
+        #endregion
+
+        /// <summary>
+        /// Launches chrome browser displaying polyline map of coordinates in object
+        /// </summary>
+        public void LoadPolylineMap()
+        {
+            //Clear out HTML file
+            string BlankDoc = AppDomain.CurrentDomain.BaseDirectory + "GoogleMaps\\BlankPolylines.html";
+            string NewLoc = AppDomain.CurrentDomain.BaseDirectory + "GoogleMaps\\UpdatedPolylines.html";
+
+            File.WriteAllText(NewLoc, String.Empty);
+
+            if (CoordPath.Count == 0)
+                Process.Start("Chrome", BlankDoc);
+            else
+            {
+                string UpdatedHTML = File.ReadAllText(BlankDoc);
+                //Add Polylines/Start/End locations
+                UpdatedHTML = UpdatedHTML.Replace("*ROUTER COORDS HERE*", getPolylineData());
+                UpdatedHTML = UpdatedHTML.Replace("*START COORDS HERE*", getStartLocStr());
+                UpdatedHTML = UpdatedHTML.Replace("*END COORDS HERE*", getEndLocStr());
+
+                File.WriteAllText(NewLoc, UpdatedHTML);
+
+                //Load updated file in chrome
+                Process.Start("Chrome", NewLoc);
+            }
+        }
+
+
+        /// <summary>
+        /// Launches chrome browser displaying heat-map of coordinate points in object
+        /// </summary>
+        public void LoadHeatmap()
+        {
+            //Clear out HTML file
+            string BlankDoc = AppDomain.CurrentDomain.BaseDirectory + "GoogleMaps\\BlankHeatmap.html";
+            string NewLoc = AppDomain.CurrentDomain.BaseDirectory + "GoogleMaps\\UpdatedHeatmap.html";
+
+            File.WriteAllText(NewLoc, String.Empty);
+
+            //Grab HTML file with no coordinates
+            string UpdatedHTML = File.ReadAllText(BlankDoc);
+
+            //If no coords saved -> load blank document
+            if(CoordPath.Count == 0)
+                Process.Start("Chrome", BlankDoc);
+            else
+            {
+                //Add coords to file
+                UpdatedHTML = UpdatedHTML.Replace("*ROUTER COORDS HERE*", getHeatmapData());
+                File.WriteAllText(NewLoc, UpdatedHTML);
+
+                //Load HTML in Chrome
+                Process.Start("Chrome", NewLoc);
+            }
+        }
+    }
+}
---
+++ NetworksFinal/MainWindow.xaml.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+
+namespace NetworksFinal
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            HeatMapInstructions.Text = "To see a heat-map of your internet traffic press the button below.";
+        }
+
+        //Loads trace route map of user input IP/Domain/URL
+        private void submit_btn_Click(object sender, RoutedEventArgs e)
+        {
+            data.Text = String.Empty;
+            Path Locations = new Path();
+            runTraceRT(WebAddress.Text, ref Locations);
+            Locations.LoadPolylineMap();
+        }
+
+        //Loads heat-map of computer's DNS cache
+        private void getHeatData_btn_Click(object sender, RoutedEventArgs e)
+        {
+            //Grab DNS cache
+            string DisplayDNSData = getCachedDNS();
+            List<string> IPAddresses = new List<string>();
+
+            //Parse DNS cache
+            for(int i = 0; i < DisplayDNSData.Length - 11; i++)
+            {
+                if (IPAddresses.Count > 15)
+                    break;
+                string sub = DisplayDNSData.Substring(i, 11);
+                
+                if (sub == "Record Name")
+                {
+                    i += 24; //length of "Record Name . . . . . : "
+                    for(int j = i; true; j++)
+                    {
+                        if(DisplayDNSData[j] == '\n')
+                        {
+                            IPAddresses.Add(DisplayDNSData.Substring(i, j - i - 1) + " ");
+                            i = j;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            //For more diverse data uncomment following line
+            //IPAddresses = IPAddresses.Distinct().ToList();
+
+            //BEGIN: Running tracert on all domains
+            List<Thread> threads = new List<Thread>();
+            Path Locs = new NetworksFinal.Path();
+            
+
+            //create threads
+            data.Text = "Domains mapped: \n";
+            foreach (string DestIP in IPAddresses)
+            {
+                threads.Add(new Thread(delegate () { runTraceRT(DestIP, ref Locs); }));
+                data.Text += DestIP + Environment.NewLine;
+            }
+
+            
+            //Start each thread
+            foreach (Thread currThread in threads)
+                currThread.Start();
+
+            //Due to the limitations on # of requests per minute I need to kill my threads before 150 requests are made
+            //If unlimited # of requests, change while loop condition to 'threads.Count != 0'
+            while (Locs.getNumCoords() < 75 && threads.Count != 0)
+            {
+                for (int j = 0; j < threads.Count; j++)
+                {
+                    if (threads[j].IsAlive == false)
+                    {
+                        threads.RemoveAt(j);
+                    }
+                }
+            }
+
+            //Abort remaining threads
+            foreach (Thread currThread in threads)
+                currThread.Abort();
+
+            Locs.LoadHeatmap();
+
+        }
+        
+        /// <summary>
+        /// Pulls computer's DNS cache and returns it as a string
+        /// </summary>
+        /// <returns>results from ipconfig /displaydns as a string</returns>
+        private string getCachedDNS()
+        {
+            
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+
+            //Setup coommand info
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/c ipconfig /displaydns";    //Run displaydns command
+            startInfo.RedirectStandardOutput = true;
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = true;
+
+            //Execute process
+            process.StartInfo = startInfo;
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd(); //Save process output
+            process.WaitForExit();
+
+            return output;
+        }
+
+        /// <summary>
+        /// Runs tracert on given IP/URL/Domain
+        /// </summary>
+        /// <param name="DestIP">IP/URL/Domain to run tracert on</param>
+        /// <param name="Locations">Object to store coordinate locations of each node in the trace route</param>
+        void runTraceRT(string DestIP, ref Path Locations)
+        {
+            //Run Tracert on given destination IP
+            IEnumerable<IPAddress> TraceRT = TraceRoute.GetTraceRoute(DestIP.Trim());
+
+            //Get all path node's coordinate locations
+            foreach (var CurrNodeIP in TraceRT)
+            {
+                Tuple<double, double> Coord = GetCoordinates(Convert.ToString(CurrNodeIP));
+                if (Coord.Item1 != 0 && Coord.Item2 != 0)
+                    Locations.AddLoc(Coord.Item1, Coord.Item2);
+            }
+        }
+        
+        /// <summary>
+        /// Uses ip-api.com to grab data on given URL/Domain/IP and parses it for the lat,lng point
+        /// </summary>
+        /// <param name="url">URL/Domain/IP that coordinates need to be grabbed</param>
+        /// <returns>Tuple of (lat,lng)</returns>
+        private Tuple<double,double> GetCoordinates(string url)
+        {
+            //Grab JSON data for current IP/Url/Domain from the ip-api website
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://ip-api.com/json/" + url); //API Documentation: http://ip-api.com/docs/api:json
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            string IP_API_Response;
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                //The Newtonsoft needs brackets around the JSON to parse it
+                IP_API_Response = "[" + reader.ReadToEnd() + "]";
+            }
+
+            //Parse JSON
+            dynamic result = JArray.Parse(IP_API_Response);
+
+
+            if (result[0].status == "success")  //if mappable IP -> add to list
+            {
+                double lon = Convert.ToDouble(result[0].lon);
+                double lat = Convert.ToDouble(result[0].lat);
+                return new Tuple<double, double>(lat, lon);
+            }
+            else
+                return new Tuple<double, double>(0, 0); //else -> set to arbitrary lon/lat to be removed later
+        }
+    }
+}
